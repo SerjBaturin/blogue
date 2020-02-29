50d8197a456c7893cbc5a8f6912d37ac31cf580b
serj <sergejbaturin1985@gmail.com>
2020-03-01 00:33:47 +0500

    Add some styles to Main Post Component

--- app/src/Blog/containers/BlogMainPost/BlogMainPost.jsx
+++ app/src/Blog/containers/BlogMainPost/BlogMainPost.jsx
@@ -2,10 +2,19 @@ import React from "react";
 import "./style.scss";
 
 const BlogMainPost = props => {
+  const { title, img, text, author, date } = props.mainPost;
   return (
-    <div className="Blog__Main_Post">
-      <h1>{props.mainPost.title}</h1>
-      <p>{props.mainPost.text}</p>
+    <div className="Blog__Main__Post">
+      <div
+        className="Main__Post__img__wrapper"
+        style={{ backgroundImage: `url(${img})` }}
+      >
+        {/* <img src={img} alt="main-post-image" /> */}
+      </div>
+      <h1>{title}</h1>
+      <p>{text}</p>
+      <b>{author}</b>
+      <span>{date}</span>
     </div>
   );
 };
